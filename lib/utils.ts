65dab541dd0ff00534a444c53ecf1ead2723ebd8
owenliversidge-optimizely <Owen.Liversidge@optimizely.com>
2025-07-03 15:37:39 +0100

    update dockerfile again

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,20 +1,5 @@
-import { ReadonlyURLSearchParams } from 'next/navigation'
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export const createUrl = (
-  pathname: string,
-  params: URLSearchParams | ReadonlyURLSearchParams
-) => {
-  const paramsString = params.toString()
-  const queryString = `${paramsString.length ? '?' : ''}${paramsString}`
-
-  return `${pathname}${queryString}`
-}
-
-export const leadingSlashUrlPath = (pathname: string) => {
-  return `${pathname.startsWith('/') ? '' : '/'}${pathname}`
-}
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
