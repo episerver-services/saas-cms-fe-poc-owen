80bc906034c1dc592e7728cad3742c1cbdf1c23f
owenliversidge-optimizely <Owen.Liversidge@optimizely.com>
2025-07-03 17:41:29 +0100

    put eslint ognores back

--- eslint.config.ts
+++ eslint.config.ts
@@ -10,6 +10,11 @@ const compat = new FlatCompat({
 })
 
 const eslintConfig = [
+  // Ignore these files
+  {
+    ignores: ['lib/optimizely/sdk.ts', 'app/globals.css'],
+  },
+
   // Apply the base config
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
 
